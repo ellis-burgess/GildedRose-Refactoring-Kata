31d9b077bbdcf68b950409ec953a20bf1d29783e
ellis-burgess <ellis.burgess@bjss.com>
2024-10-09 14:09:36 +0100

    Extract calculate quality change function

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -36,20 +36,29 @@ func UpdateSellInDate(item *Item) {
 	item.SellIn -= 1
 }
 
-func UpdateQuality(item *Item) {
+func CalculateQualityChange(item *Item) int {
 	qualityChange := BASE_QUALITY_CHANGE
+
+	if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+		return 3 - (item.SellIn / 5)
+	}
 
 	if item.SellIn < 0 {
 		qualityChange = qualityChange * 2
 	}
 
+	return qualityChange
+}
+
+func UpdateQuality(item *Item) {
+	qualityChange := CalculateQualityChange(item)
+
 	switch name := item.Name; name {
 	case "Backstage passes to a TAFKAL80ETC concert":
-		if item.SellIn >= 0 {
-			qualityChange = 3 - (item.SellIn / 5)
-			item.Quality = CeilingAdd(item.Quality, qualityChange, MAX_QUALITY)
+		if item.SellIn < 0 {
+			item.Quality = MIN_QUALITY
 		} else {
-			item.Quality = 0
+			item.Quality = CeilingAdd(item.Quality, qualityChange, MAX_QUALITY)
 		}
 	case "Aged Brie":
 		item.Quality = CeilingAdd(item.Quality, qualityChange, MAX_QUALITY)
